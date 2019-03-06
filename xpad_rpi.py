73f6fd12f7d08fe611fee209f450e6467d04d2a9
William Su <wilson289296@gmail.com>
2019-03-06 01:26:06 -0800

    functional updates

--- xpad_rpi.py
+++ xpad_rpi.py
@@ -1,6 +1,12 @@
 #import evdev
 from evdev import InputDevice, categorize, ecodes
 
+#serial prep
+import serial
+port = "/dev/ttyACM0"
+ser = serial.Serial(port,9600)
+ser.flushInput()
+
 #creates object 'gamepad' to store the data
 gamepad = InputDevice('/dev/input/event0')
 
@@ -33,16 +39,20 @@ righttrig = 05
 
 #prints info at start
 print(gamepad)
-
 #evdev takes care of polling controller in loop
 for event in gamepad.read_loop():
+   print(event)
    if event.type == ecodes.EV_KEY:
 	if event.value == 1:
 	    if event.code == abtn:
 		print("A")
+		ser.write('A')
 	    if event.code == bbtn:
 		print("B")
+		ser.write('B')
 	    if event.code == xbtn:
 		print("X")
+		ser.write('X')
 	    if event.code == ybtn:
 		print("Y")
+		ser.write('Y')
